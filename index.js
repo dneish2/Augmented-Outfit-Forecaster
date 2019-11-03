7916a7d24ef717485ca4fd40bb9b013b824a9fe3
dneish2 <47768835+dneish2@users.noreply.github.com>
2019-11-03 01:18:52 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,28 @@
 //Dark Sky Api Requests
+<body onload="getLocation()"></body>
+
+<p id="demo"></p>
+
+<script>
+var x = document.getElementById("demo");
+var coordinates = "";
+
+function getLocation() {
+ if (navigator.geolocation) {
+   navigator.geolocation.getCurrentPosition(showPosition);
+ } else {
+   x.innerHTML = "Geolocation is not supported by this browser.";
+ }
+}
+
+function showPosition(position) {
+ // x.innerHTML = "Latitude: " + position.coords.latitude +
+ // "<br>Longitude: " + position.coords.longitude; -->
+coordinates = position.coords.latitude + "," + position.coords.longitude;
+}
+
+//// API FETCHING
+
 
 let fetch = require('node-fetch');
 
@@ -27,3 +51,4 @@ fetch(req)
   .then (j) =>{
     console.log('JSON data provided');
   })
+  </script>
