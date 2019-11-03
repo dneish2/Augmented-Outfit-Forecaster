bcc0e4d6d49aaec59a9d70b1d51a8a349a012ffc
dneish2 <47768835+dneish2@users.noreply.github.com>
2019-11-03 06:04:12 -0500

    Update index.js

--- index.js
+++ index.js
@@ -62,7 +62,7 @@ if (condition == 'rain' || condition == 'snow' || condition == 'sleet' || condit
   extra = 1;
 }
 
-let file = "User Level Is: " level + "\n" "User Extra: ""+ extra;
+let file = "User Level Is: "level + "\n" "User Extra: ""+ extra;
 
 // Write data in 'Output.txt' .
 fs.writeFile('Clothes.txt', file, (err) => {
