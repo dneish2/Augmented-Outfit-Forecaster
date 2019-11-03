5a7e676594dfeb7344b84775f89903b11d3d1aa0
dneish2 <47768835+dneish2@users.noreply.github.com>
2019-11-03 01:17:10 -0500

    test

--- index.js
+++ index.js
@@ -1,17 +1,29 @@
-<script>
-var x = document.getElementById("demo");
-var coordinates = "";
-function getLocation() {
-  if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(showPosition);
-  } else {
-    x.innerHTML = "Geolocation is not supported by this browser.";
-  }
+//Dark Sky Api Requests
+
+let fetch = require('node-fetch');
+
+let darksky = 'https://api.darksky.net/forecast/c9eff229d9e3b4b0f65ce1f916ee8145/';
+
+let uri = darksky + '/' + coordinates;
+console.log(uri);
+
+let options = {
+  method: 'GET'.
+  mode: 'cors'
 }
 
-function showPosition(position) {
-  x.innerHTML = "Latitude: " + position.coords.latitude +
-  "<br>Longitude: " + position.coords.longitude;
-  coordinates = position.coords.latitude + "," + position.coords.longitude;
-}
-</script>
+let req = new fetch.Request(uri, options);
+
+fetch(req)
+  .then((response)=>{
+    if(response.ok){
+      return response.json();
+    }else{
+      throw new Error('BAD HTTP DUDE!')
+
+    }
+    }
+  })
+  .then (j) =>{
+    console.log('JSON data provided');
+  })
