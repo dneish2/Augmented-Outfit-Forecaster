8141f092e779faaf6ddd294f0115c10bda2d37c9
dneish2 <47768835+dneish2@users.noreply.github.com>
2019-11-03 02:29:32 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 //JavaScript File
 //Dark Sky Api Requests
-<script>
-let fetch = require('node-fetch');
+
+
 var x = document.getElementById("demo");
 var coordinates = "";
 
@@ -17,8 +17,18 @@ function showPosition(position) {
  // x.innerHTML = "Latitude: " + position.coords.latitude +
  // "<br>Longitude: " + position.coords.longitude; -->
 coordinates = position.coords.latitude + "," + position.coords.longitude;
+
+const api = `https://api.darksky.net/forecast/c9eff229d9e3b4b0f65ce1f916ee8145/${coordinates}`;
+
+fetch (api)
+.then(response => {
+  return response.json();
+})
+.then(data => {
+  console.log(data);
+});
 }
-</script>
+
 //// API FETCHING
 /*
 let fetch = require('node-fetch');
