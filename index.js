77fa64082d80b03e2aa5e82500e22127465c1636
dneish2 <47768835+dneish2@users.noreply.github.com>
2019-11-03 02:23:29 -0500

    Update index.js

--- index.js
+++ index.js
@@ -18,21 +18,7 @@ function showPosition(position) {
  // "<br>Longitude: " + position.coords.longitude; -->
 coordinates = position.coords.latitude + "," + position.coords.longitude;
 
-const api = `https://api.darksky.net/forecast/c9eff229d9e3b4b0f65ce1f916ee8145/${coordinates}`;
-
-fetch(api)
-.then(response => {
-  return response.json();
-})
-.then(data =>{
-  console.log(data);
-});
 }
-
-
-
-</script>
-//// API FETCHING
 /*
 let fetch = require('node-fetch');
 
