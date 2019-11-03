67930e26558adbee7835b70159c4bbe39a64063f
dneish2 <47768835+dneish2@users.noreply.github.com>
2019-11-03 06:41:27 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 //JavaScript File
 //Dark Sky Api Requests
-//File Write Require
-var fs = require('fs');
+
 var x = document.getElementById("demo");
 var coordinates = "";
 var fahrenheit = "";
@@ -62,44 +61,4 @@ if (condition == 'rain' || condition == 'snow' || condition == 'sleet' || condit
 }
 })
 
-
-
 }
-
-
-  //Declare Temperature Ranges For internal use
-  //if (fahrenheit )
-
-
-
-
-//// API FETCHING
-/*
-let fetch = require('node-fetch');
-
-//let darksky = 'https://api.darksky.net/forecast/c9eff229d9e3b4b0f65ce1f916ee8145/';
-
-let uri = darksky + '/' + coordinates;
-console.log(uri);
-
-let options = {
-  method: 'GET'.
-  mode: 'cors'
-}
-
-let req = new fetch.Request(uri, options);
-
-fetch(req)
-  .then((response)=>{
-    if(response.ok){
-      return response.json();
-    }else{
-      throw new Error('BAD HTTP DUDE!')
-
-    }
-    }
-  })
-  .then (j) =>{
-    console.log('JSON data provided');
-  })
-  */
