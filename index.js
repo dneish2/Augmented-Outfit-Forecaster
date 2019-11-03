88dc2e18cb62caf68718b6cf9f75342d56ebacd3
dneish2 <47768835+dneish2@users.noreply.github.com>
2019-11-03 01:58:35 -0400

    Update index.js

---
+++ index.js
@@ -0,0 +1,17 @@
+<script>
+var x = document.getElementById("demo");
+var coordinates = "";
+function getLocation() {
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(showPosition);
+  } else {
+    x.innerHTML = "Geolocation is not supported by this browser.";
+  }
+}
+
+function showPosition(position) {
+  x.innerHTML = "Latitude: " + position.coords.latitude +
+  "<br>Longitude: " + position.coords.longitude;
+  coordinates = position.coords.latitude + "," + position.coords.longitude;
+}
+</script>
