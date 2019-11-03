9e63cba2577c44ca68006387edca76855fadda23
dneish2 <47768835+dneish2@users.noreply.github.com>
2019-11-03 07:18:14 -0500

    Update index.js

--- index.js
+++ index.js
@@ -61,61 +61,12 @@ if (condition == 'rain' || condition == 'snow' || condition == 'sleet' || condit
   extra = 1;
 }
 
-var myObject = { name: "simon", surname: "goellner" };
-// convert to json string
-var myJSON = JSON.stringify( myObject );
+var fs = require('fs');
 
-var $link = $("<a />");
-// encode any special characters in the JSON
-var text = encodeURIComponent( myJSON );
-
-// <a download="filename.txt" href='data:application/octet-stream,...'></a>
-$link
-  .attr( "download", "filename.txt" )
-  .attr( "href", "data:application/octet-stream," + text )
-  .appendTo( "body" )
-  .get(0)
-  .click()
+//create a file named mynewfile3.txt:
+fs.writeFile('mynewfile3.txt', 'Hello content!', function (err) {
+  if (err) throw err;
+  console.log('Saved!');
+});
 
 }
-
-
-
-
-
-  //Declare Temperature Ranges For internal use
-  //if (fahrenheit )
-
-
-
-
-//// API FETCHING
-/*
-let fetch = require('node-fetch');
-
-//let darksky = 'https://api.darksky.net/forecast/c9eff229d9e3b4b0f65ce1f916ee8145/';
-
-let uri = darksky + '/' + coordinates;
-console.log(uri);
-
-let options = {
-  method: 'GET'.
-  mode: 'cors'
-}
-
-let req = new fetch.Request(uri, options);
-
-fetch(req)
-  .then((response)=>{
-    if(response.ok){
-      return response.json();
-    }else{
-      throw new Error('BAD HTTP DUDE!')
-
-    }
-    }
-  })
-  .then (j) =>{
-    console.log('JSON data provided');
-  })
-  */
